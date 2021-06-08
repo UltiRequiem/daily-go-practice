b1ee32d0096674b7e58a48acff56bd22b628e728
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-08 11:22:29 -0500

    :art: Update

---
+++ go-training/07-errorsHandler/customError/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func checkUserNameExist(username string) (bool, error) {
+	if username == "UltiRequiem" {
+		return true, ErrUserNameExist{UserName: username}
+	}
+
+	return false, nil
+}
+
+func main() {
+	if _, err := checkUserNameExist("foo"); err == nil {
+		fmt.Println("foo not exist")
+	}
+
+	if _, err := checkUserNameExist("UltiRequiem"); err != nil {
+		fmt.Println(err.Error())
+	}
+
+	if _, err := checkUserNameExist("UltiRequiem"); err != nil {
+		if ok := IsErrUserNameExist(err); ok {
+			fmt.Println("user appleboy already exist.")
+		}
+	}
+}
