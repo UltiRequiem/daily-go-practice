744ba0692386badc3ad420a073fdd40b3d2a462c
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-16 08:52:46 -0500

    :art: Update

---
+++ go-training/15-functionArgs/append/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func addValue(foo []string) {
+	foo = append(foo, "c")
+	fmt.Println("modify foo", foo)
+}
+
+func main() {
+	foo := []string{"a", "b"}
+	fmt.Println("before foo:", foo)
+	addValue(foo)
+	fmt.Println("after foo:", foo)
+	bar := foo[:1]
+	fmt.Println("bar:", bar)
+	s1 := append(bar, "c")
+	fmt.Println("foo:", foo)
+	fmt.Println("s1:", s1)
+	s2 := append(bar, "d")
+	fmt.Println("foo:", foo)
+	fmt.Println("s2:", s2)
+	s3 := append(bar, "e", "f")
+	fmt.Println("foo:", foo)
+	fmt.Println("s3:", s3)
+}
---
+++ go-training/15-functionArgs/slice-arg/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func modify(foo []string) []string {
+	foo[1] = "c"
+	fmt.Println("modify foo", foo)
+	return foo
+}
+
+func main() {
+	foo := []string{"a", "b"}
+	fmt.Println("before foo:", foo)
+	modify(foo)
+	fmt.Println("after foo:", foo)
+}
