ee9931a6353384e2123834fc72c8f012e16ea56d
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-20 11:42:29 -0500

    :art: Update

---
+++ go-training/19-kubernetes/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var version = "master"
+
+func showVersion(w http.ResponseWriter, r *http.Request) {
+	log.Println(version)
+	w.Write([]byte(version))
+}
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	message := r.URL.Path
+	message = strings.TrimPrefix(message, "/")
+	message = "Hello, got the message: " + message
+	log.Println(message)
+	w.Write([]byte(message))
+}
+
+func main() {
+	port := "8080"
+	if fromEnv := os.Getenv("PORT"); fromEnv != "" {
+		port = fromEnv
+	}
+	http.HandleFunc("/version", showVersion)
+	http.HandleFunc("/", sayHello)
+	log.Println("Listen server on " + port + " port")
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
