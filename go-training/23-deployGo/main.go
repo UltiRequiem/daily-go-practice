18635c0e6cb8e9cdd3753f076271765b4565d27a
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-24 16:09:44 -0500

    :sparkles: Day 23

---
+++ go-training/23-deployGo/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := ":" + os.Getenv("PORT")
+	stage := os.Getenv("UP_STAGE")
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong " + stage,
+		})
+	})
+
+	r.GET("/v1", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong " + stage + " v1 ++ drone",
+		})
+	})
+
+	r.GET("/v2", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong " + stage + " v2 ++ production",
+		})
+	})
+
+	r.GET("/v3", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong " + stage + " v3 ++ production",
+		})
+	})
+
+	r.Run(port)
+}
