8dafd5d535c7872cc8c2713a8a2dfaf322c199c2
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-07-25 20:24:55 -0500

    :sparkles: Day 54

---
+++ learngo/05-writeYourFirstLibraryPackage/exercise/cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/UltiRequiem/learngo/05-writeYourFirstLibraryPackage/exercise/"
+)
+
+func main() {
+	fmt.Println(golang.Version())
+}
---
+++ learngo/05-writeYourFirstLibraryPackage/exercise/main.go
@@ -0,0 +1,10 @@
+package golang
+
+import (
+	"runtime"
+)
+
+// Version returns the current Go version
+func Version() string {
+	return runtime.Version()
+}
